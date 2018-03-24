b61575895fe064b37904ecb16d2223c8dec1365f
Prithu Goswami <prithugoswami524@gmail.com>
2018-03-24 13:08:03 +0530

    update install.sh
    The install script copies the files to the home directory directly

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+    ln -s $dir/$file ~
 done
 
 source ~/.bashrc
