8923e58fa4caf9a181578df502bbffdfe473a49b
prithugoswami <prithugoswami524@gmail.com>
2018-06-13 18:25:20 +0530

    update scripts/toggle_touchpad.sh

--- scripts/toggle_touchpad.sh
+++ scripts/toggle_touchpad.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # device id=14
-# toggles the touchapd on and sleeps for 30 seconds and then
-# disables it again.
 touchpad="$(xinput list-props 14 | awk 'NR == 2 {print $4}')"
 
 if [ "$touchpad" -eq 0 ]; then
