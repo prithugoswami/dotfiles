3a228a1a58d1b878309fa9e47f387d1e2a20db29
Prithu Goswami <pg@prithu.dev>
2023-06-12 14:49:01 +0530

    nvim: add new nvim-tree mapping options

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,8 +5,19 @@ require 'completion'
 require 'lsp'
 
 -- TODO move each plugin setup to own file
+local function nvim_tree_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', 't', api.node.open.tab, opts('Open: New Tab'))
+  vim.keymap.set('n', '<C-s>', api.node.open.horizontal, opts('Open: Horizontal Split'))
+end
+
 
 require("nvim-tree").setup({
+  on_attach = nvim_tree_on_attach,
   sort_by = "case_sensitive",
   filters = {
     custom = {"__pycache__"}
@@ -34,14 +45,6 @@ require("nvim-tree").setup({
           deleted = "✗",
           ignored = "◌",
         }
-      }
-    }
-  },
-  view = {
-    mappings = {
-      list = {
-        { key = "t", action = "tabnew"},
-        { key = "<C-s>", action = "split"}
       }
     }
   },
