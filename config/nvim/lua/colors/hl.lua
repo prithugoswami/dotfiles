474a6f1e6a98f8fb35ab3af29f4eb746f7a6d592
Prithu Goswami <pg@prithu.dev>
2024-12-19 01:52:13 +0530

    use truecolors

--- config/nvim/lua/colors/hl.lua
+++ config/nvim/lua/colors/hl.lua
@@ -2,5 +2,5 @@ local hl = vim.api.nvim_set_hl
 
 hl(0, 'TabLine', { fg = '#7e7e7e' })
 hl(0, 'TabLineSel', { bold = true })
-hl(0, 'NvimTreeNormal', { bg = '#0c0c0c' })
-hl(0, 'NvimTreeWinSeparator', { bg = '#0c0c0c', fg = '#0c0c0c' })
+hl(0, 'NvimTreeNormal', { bg = '#0f0f0f' })
+hl(0, 'NvimTreeWinSeparator', { bg = '#0f0f0f', fg = '#0f0f0f' })
